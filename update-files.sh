af89f3951e2fd8217e1d36e3ae1610565473bf34
Facundo Farias <faf@changeyourflight.com>
2014-11-26 17:07:28 +0100

    Added new languages. Updated shell script

---
+++ update-files.sh
@@ -0,0 +1,5 @@
+# csvkit should be installed (https://csvkit.readthedocs.org)
+csvjson -i 4 de/Airports.csv > de/Airports.json
+csvjson -i 4 en/Airports.csv > en/Airports.json
+csvjson -i 4 es/Airports.csv > es/Airports.json
+csvjson -i 4 fr/Airports.csv > fr/Airports.json
